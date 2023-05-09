f4761ced39d6e1763b8a263b6b76e0df62e9744f
Igor Bondarchuk <bondatchuk1989@gmail.com>
2023-05-09 16:43:21 +0300

    It was deleted

--- src/components/users/tools/tools.js
+++
@@ -1,4 +0,0 @@
-export const gridTools = {
-  spacing: { xs: 2, md: 3 },
-  columns: { xs: 2, sm: 6, md: 12 },
-};
--- src/redux/initialState.js
+++
@@ -1,5 +0,0 @@
-export const initialState = {
-  users: [],
-  isLoading: false,
-  error: null,
-};
